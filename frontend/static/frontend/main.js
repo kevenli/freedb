8a671ad314e6c7046048cfdcd226230175be9a48
KevenLi <pbleester@gmail.com>
2020-06-22 10:51:06 +0800

    frontend use relative api path, and session authentication.

--- frontend/static/frontend/main.js
+++ frontend/static/frontend/main.js
@@ -3706,7 +3706,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ \"./node_modules/axios/index.js\");\n/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (axios__WEBPACK_IMPORTED_MODULE_0___default.a.create({\n  baseURL: \"http://localhost:8000/api\",\n  headers: {\n    \"Content-type\": \"application/json\",\n    \"Authorization\": \"Basic YWRtaW46MTIzNDU2\"\n  }\n}));\n\n//# sourceURL=webpack:///./src/http-common.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ \"./node_modules/axios/index.js\");\n/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (axios__WEBPACK_IMPORTED_MODULE_0___default.a.create({\n  baseURL: \"/api\",\n  headers: {\n    \"Content-type\": \"application/json\"\n  }\n}));\n\n//# sourceURL=webpack:///./src/http-common.js?");
 
 /***/ }),
 
