1607a41d9bb47f7b803b30ec84dff9f6db943e76
KevenLi <pbleester@gmail.com>
2020-07-10 10:51:47 +0800

    v0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='freedb',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     package_data = {
         'frontend': [
